5311e375f834e5e587e6d5c92a7ca7c32be35e70
Yusuke ISHIGAMI <i.usk1n.pro@gmail.com>
2019-10-04 23:02:10 +0900

    toppageのマッピングを修正

--- src/main/java/com/market/sapphires/sbt/controller/TopPageController.java
+++ src/main/java/com/market/sapphires/sbt/controller/TopPageController.java
@@ -4,14 +4,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/toppage")
+@RequestMapping(value = "/toppage")
 public class TopPageController {
 
-    @RequestMapping(value = "/")
-    private String list(Model model) {
+    @GetMapping(value = { "", "/", "list" })
+    public String list(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("username", auth.getName());
         return "toppage";
