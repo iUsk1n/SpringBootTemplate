babd8a7fa27402bea1c3ef32de7f8ae9d17730db
Yusuke ISHIGAMI <i.usk1n.pro@gmail.com>
2019-10-13 08:34:26 +0900

    MessageUtilの実装

--- src/main/java/com/market/sapphires/sbt/controller/PortalController.java
+++ src/main/java/com/market/sapphires/sbt/controller/PortalController.java
@@ -1,13 +1,27 @@
 package com.market.sapphires.sbt.controller;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.market.sapphires.sbt.util.MessageUtil;
+
 @Controller
 @RequestMapping(value = "/portal")
 public class PortalController {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @PostConstruct
+    public void init() {
+        MessageUtil.setMessageSource(this.messageSource);
+    }
 
     @GetMapping(value = { "", "/" })
     public String index(Model model) {
